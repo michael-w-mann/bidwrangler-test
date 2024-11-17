0e3012cc2a5481ab35c1d30a67053d4ecd1a5aa0
Michael Mann <michaelmannw@gmail.com>
2024-11-17 15:56:58 -0500

    Rename bid-wrangler-demo directory to backend for clarity

--- bid-wrangler-demo/app/channels/auction_channel.rb
+++
@@ -1,9 +0,0 @@
-class AuctionChannel < ApplicationCable::Channel
-  def subscribed
-    stream_from "auction_channel"
-  end
-
-  def unsubscribed
-    # Any cleanup needed when channel is unsubscribed
-  end
-end
--- bid-wrangler-demo/app/models/item.rb
+++
@@ -1,6 +0,0 @@
-class Item < ApplicationRecord
-    has_many :bids, dependent: :destroy
-    
-    validates :name, presence: true
-    validates :current_price, presence: true, numericality: { greater_than_or_equal_to: 0 }
-  end
